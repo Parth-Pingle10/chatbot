be8785615743d4fba8fcbd71919ecaf9d2a1eaa2
Parth-Pingle10 <onlynonlyminecraft@gmail.com>
2025-06-27 01:41:12 +0530

    chatbot

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Optional: fallback route
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/chatbot.html'));
 });
 
 mongoose.connect(process.env.MONGODB_URI
