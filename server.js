e2bf01c2c3470cbf3dba0adf0c115ff26816965b
Parth-Pingle10 <onlynonlyminecraft@gmail.com>
2025-06-27 01:58:46 +0530

    ok

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Fallback route - serve chatbot.html
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'chatbot.html'));
 });
 
